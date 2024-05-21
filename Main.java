092e48918fccb7953af141f2bd37b87aa1435b97
YuckG <yuckguedes@gmail.com>
2024-05-21 19:16:55 -0300

    commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+	public static void main(String[] args) {
+		Tests tests = new Tests();
+		tests.testMultiplication();
+		tests.testEquality();
+		tests.testCurrency();
+	}
+}
--- Money.java
+++ Money.java
@@ -1,18 +1,30 @@
-abstract class Money  {
-    protected int amount;
-    
-    public boolean equals(Object object) {
-        Money money = (Money) object;
-        return amount == money.amount && getClass().equals(money.getClass());
-     }
+class Money  {
+	protected int amount;
+	protected String currency;
 
-     abstract Money times(int multiplier);  
+	Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
 
-     static Money dollar(int amount)  {
-        return new Dollar(amount);
-     }
+	static Money dollar(int amount)  {
+		return new Money(amount, "USD");
+	}
 
-     static Money franc(int amount) {
-        return new Franc(amount);
-      }
- }
+	static Money franc(int amount) {
+		return new Money(amount, "CHF");
+	}
+
+	public boolean equals(Object object)  {
+		Money money = (Money) object;
+		return amount == money.amount && currency().equals(money.currency());
+	}
+
+	Money times(int multiplier) {
+		return new Money(amount * multiplier, currency);
+	}
+
+	String currency() {
+		return currency;
+	}
+}
